a9e09da846480de6ac42fc2b7cb8cf940020c0a1
fairfairfa <fairfair4545@gmail.com>
2025-08-09 20:10:46 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/aom/",              // เพิ่มบรรทัดนี้
   server: {
     host: "::",
     port: 8080,
